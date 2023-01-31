3ba68d6f2517444682e25f42d6e5cc29a222d702
sam weiss <samweiss2251@gmail.com>
2023-01-30 19:29:05 -0500

    adjust the size

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import streamlit as st
 import base64
 
-    
-def add_bg_from_local(image_file):
+
+col1, col2, col3 = st.columns(3)
+image_file = "sam weiss.jpg"
+main = col2
+with col2:
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
+        f"""
+       <style>
+       .stApp {{
+           background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+           width: 100%;
+           height: 850px;
+           no-repeat center;
+           background-size: cover;
+       }}
+       </style>
+       """,
+        unsafe_allow_html=True
     )
-add_bg_from_local('sam weiss.jpg')
-
-
-
-
 
