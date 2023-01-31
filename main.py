135a0779bb86ef32cfb2a994fc56412a748dc3dd
sam weiss <samweiss2251@gmail.com>
2023-01-30 19:35:17 -0500

    adjust the size

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import base64
 col1, col2, col3 = st.columns(3)
 image_file = "sam weiss.jpg"
 main = col2
+with col1:
+    st.write("this is col 1")
+with col3:
+    st.write("this is the third col")
 with col2:
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
@@ -13,8 +17,8 @@ with col2:
        <style>
        .stApp {{
            background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-           width: 100%;
-           height: 850px;
+           width: 99%;
+           height: 70%;
            no-repeat center;
            background-size: cover;
        }}
